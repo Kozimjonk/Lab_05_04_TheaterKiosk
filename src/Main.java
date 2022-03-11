33f541f439252060c6d2b6aca97aefef5b97ec5d
Kozimjon <kozimkuchkarov77@gmail.com>
2022-03-10 19:22:17 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println("enter your age");
+        int age = 24;
+        if ( age >= 21)
+        {
+            System.out.println("You get a wristband!");
+        }
+
+    }
+}
